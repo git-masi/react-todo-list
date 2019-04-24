58b9049cf9665f4046907f9403f20f2128142349
Eric Masi <eric@gitmasi.com>
2019-04-24 14:32:51 -0400

    add logic to allow todo edit

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,9 +5,14 @@ class Todo extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      complete: false
+      complete: false,
+      edit: false,
+      text: this.props.todoText
     }
     this.completeHandler = this.completeHandler.bind(this);
+    this.editHandler = this.editHandler.bind(this);
+    this.changeHandler = this.changeHandler.bind(this);
+    this.enterKeyHandler = this.enterKeyHandler.bind(this);
   }
 
   completeHandler() {
@@ -15,12 +20,38 @@ class Todo extends Component {
     this.setState({complete: toggleComplete})
   }
 
+  editHandler() {
+    const toggleEdit = !this.state.edit;
+    this.setState({edit: toggleEdit});
+  }
+
+  changeHandler(e) {
+    this.setState({[e.target.name]: e.target.value}); 
+  }
+
+  enterKeyHandler(e) {
+    if (e.key === 'Enter') this.setState({edit: false, text: e.target.value});
+  }
+
   render() {
     return (
       <div className="Todo">
-        <span onClick={this.completeHandler} className={this.state.complete ? 'complete' : null}>{this.props.todoText}</span>
+        <input
+          type="text"
+          name="text"
+          onChange={this.changeHandler}
+          onKeyPress={this.enterKeyHandler}
+          value={this.state.text} 
+          style={{display: this.state.edit ? 'inline-block' : 'none'}}>
+        </input>
+        <span 
+          onClick={this.completeHandler}
+          className={this.state.complete ? 'complete' : null}
+          style={{display: this.state.edit ? 'none' : 'inline-block'}}>
+          {this.state.text}
+        </span>
         <div>
-          <i className="fas fa-pencil-alt"></i>
+          <i onClick={this.editHandler} className="fas fa-pencil-alt"></i>
           <i className="fas fa-trash"></i>
         </div>
       </div>
